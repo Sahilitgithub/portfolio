8de03d81564bed3d7a184243867a6234eeddf3a5
sahil <mdhannanit2021@gmail.com>
2025-10-24 11:52:22 +0600

    next.config.ts file

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,8 @@ const nextConfig: NextConfig = {
   // this includes files from the monorepo base two directories up
   transpilePackages: ['three'],
   outputFileTracingRoot: path.join(__dirname, '../../'),
+ // Keep this official timeout for slow static page generation (if needed)
+  staticPageGenerationTimeout: 60, // Default is 60 seconds. Increase only if build fails.
   /* config options here */
   images: {
     remotePatterns: [ 

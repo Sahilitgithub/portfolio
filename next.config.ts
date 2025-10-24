ba87a5774de338d11fb94bef42e50e9543eca5fb
sahil <mdhannanit2021@gmail.com>
2025-10-24 12:13:39 +0600

    outputFileTracingRoot: path.join(__dirname, ../../), errors

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,18 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
-  // this includes files from the monorepo base two directories up
   transpilePackages: ['three'],
-  outputFileTracingRoot: path.join(__dirname, '../../'),
- // Keep this official timeout for slow static page generation (if needed)
-  staticPageGenerationTimeout: 60, // Default is 60 seconds. Increase only if build fails.
-  /* config options here */
+  staticPageGenerationTimeout: 60,
   images: {
-    remotePatterns: [ 
+    remotePatterns: [
       {
         protocol: 'https',
         hostname: 'github.com',
         port: '',
         pathname: '/**',
-      }
+      },
     ],
-  }
+  },
 };
 
 export default nextConfig;
